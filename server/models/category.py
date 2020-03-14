44a7e8978c68c5e89312fd8d49acf82ca7cd5566
chohongjae <bjq913@naver.com>
2020-03-14 17:02:16 +0900

    카테고리 모델

---
+++ server/models/category.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
