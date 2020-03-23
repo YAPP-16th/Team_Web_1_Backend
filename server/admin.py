6ee95a343c34b68a14ed0a31e2196e852998c45d
chohongjae <bjq913@naver.com>
2020-03-23 17:36:41 +0900

    user, category 모델 admin 에 추가

--- server/admin.py
+++ server/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+from server.models.category import Category
+
+User = get_user_model()
+admin.site.register(User)
+admin.site.register(Category)
