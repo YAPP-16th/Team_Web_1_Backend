96d5ce067677e644b934fc5e4359cfb62453cf4f
chohongjae <bjq913@naver.com>
2020-03-27 14:57:18 +0900

    admin에 user와 category 추가

--- server/admin.py
+++ server/admin.py
@@ -4,5 +4,11 @@ from django.contrib.auth import get_user_model
 from server.models.category import Category
 
 User = get_user_model()
-admin.site.register(User)
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'password', 'username', 'is_superuser', 'date_joined')
+
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Category)
