e25f1cbc036b6cb28cd42b1a05b3357c4d28374f
chohongjae <bjq913@naver.com>
2020-03-27 14:59:50 +0900

    유저조회시 로그인 및 본인 유저 객체만 확인할 수 있는 퍼미션 추가

---
+++ server/permissions.py
@@ -0,0 +1,26 @@
+from rest_framework import permissions
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the owner of the snippet.
+        return obj.owner == request.user
+
+
+class IsAuthenticatedAndOwner(permissions.BasePermission):
+    message = 'You must be the owner of this object.'
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return obj == request.user
