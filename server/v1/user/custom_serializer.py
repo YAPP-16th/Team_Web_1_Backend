4b906b691e9afd6a90868ca270004e41880179a2
chohongjae <bjq913@naver.com>
2020-04-03 01:36:15 +0900

    불필요한 주석 제거

--- server/v1/user/custom_serializer.py
+++ server/v1/user/custom_serializer.py
@@ -3,30 +3,18 @@ from rest_framework_jwt.serializers import (JSONWebTokenSerializer, VerifyJSONWe
 
 
 class CustomJSONWebTokenSerializer(JSONWebTokenSerializer):
-    """
-    Override rest_framework_jwt's ObtainJSONWebToken serializer to
-    force it to raise ValidationError exception if validation fails.
-    """
 
     def is_valid(self, raise_exception=True):
         return super().is_valid(raise_exception=raise_exception)
 
 
 class CustomVerifyJSONWebTokenSerializer(VerifyJSONWebTokenSerializer):
-    """
-    Override rest_framework_jwt's VerifyJSONWebToken serializer to
-    force it to raise ValidationError exception if validation fails.
-    """
 
     def is_valid(self, raise_exception=True):
         return super().is_valid(raise_exception=raise_exception)
 
 
 class CustomRefreshJSONWebTokenSerializer(RefreshJSONWebTokenSerializer):
-    """
-    Override rest_framework_jwt's VerifyJSONWebToken serializer to
-    force it to raise ValidationError exception if validation fails.
-    """
 
     def is_valid(self, raise_exception=True):
         return super().is_valid(raise_exception=raise_exception)
