edc3d94b5f2c3dffc12423e5f9441d4f0c964bce
chohongjae <bjq913@naver.com>
2020-04-09 01:27:37 +0900

    카테고리 모델 작성중, 유저별 카테고리들만의 순서를 지정하는 함수 만들어야 함.

--- server/models/category.py
+++ server/models/category.py
@@ -1,7 +1,35 @@
+from django.conf import settings
 from django.db import models
+from rest_framework import serializers
+
+
+class CustomCategoryManager(models.Manager):
+    def get_last_order(self, request):
+        return max([category.order for category in self.filter(user=request.user)])
+
+    def move(self, obj, new_order):
+        pass
 
 
 class Category(models.Model):
+    # TODO 메타정보 크롤러 있어야한다.
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='categories', on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
+    is_favorited = models.BooleanField(default=False)
+    order = models.IntegerField(default=1)  # 유저별로 순서가 달라야한다.
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    objects = CustomCategoryManager()
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+    class Meta:
+        ordering = ["is_favorited", "order"]
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
