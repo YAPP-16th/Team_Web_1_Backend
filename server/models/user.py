e335c6282a13431a15a2a6bcb8d5ebd6a98f4810
chohongjae <bjq913@naver.com>
2020-04-12 23:45:44 +0900

    유저 반환시 엑세스토큰, 리프레시 토큰 반환으로 변경

--- server/models/user.py
+++ server/models/user.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
-from rest_framework_jwt.settings import api_settings
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class CustomUserManager(BaseUserManager):
@@ -73,11 +73,11 @@ class UserSerializer(serializers.ModelSerializer):
         return user
 
     def get_token(self, instance):
-        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
-        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
-        payload = jwt_payload_handler(instance)
-        token = jwt_encode_handler(payload)
-        return token
+        refresh = RefreshToken.for_user(instance)
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }
 
 
 class UserSignInSerializer(serializers.Serializer):
