f70238842b7bed49c3b7d776387a74ff226a5750
chohongjae <bjq913@naver.com>
2020-04-15 01:53:38 +0900

    해당 객체의 owner만 true를 반환하는 퍼미션 클래스 추가

--- server/permissions.py
+++ server/permissions.py
@@ -2,19 +2,20 @@ import requests
 from rest_framework import permissions
 
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class IsOwner(permissions.BasePermission):
     """
     Custom permission to only allow owners of an object to edit it.
     """
+    message = '해당 토큰으로 참조할 수 없습니다.'
 
     def has_object_permission(self, request, view, obj):
         # Read permissions are allowed to any request,
         # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
-            return True
+        # if request.method in permissions.SAFE_METHODS:
+        #     return True
 
         # Write permissions are only allowed to the owner of the snippet.
-        return obj.owner == request.user
+        return obj.user == request.user
 
 
 class IsObjectMe(permissions.BasePermission):
