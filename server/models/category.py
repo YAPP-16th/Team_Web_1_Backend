21f6cf344292b20bea5129719b42ed604145f676
chohongjae <bjq913@naver.com>
2020-04-15 01:54:46 +0900

    나의 카테고리 중 가장 마지막 order 객체 반환 함수 수정, 더 좋은 방법이 있는지 생각해봐야함 & move 작성 중

--- server/models/category.py
+++ server/models/category.py
@@ -1,5 +1,6 @@
 from django.conf import settings
-from django.db import models
+from django.db import models, transaction
+from django.db.models import F
 from rest_framework import serializers
 
 
@@ -7,11 +8,34 @@ class CustomCategoryManager(models.Manager):
     def get_my_last_order(self, request):
         my_categories = self.filter(user=request.user)
         if my_categories:
-            return max([category.order for category in my_categories])
+            return my_categories.order_by('order').last().order
         return 0
 
-    def move(self, obj, new_order):
-        pass
+    def move(self, request, obj, new_order):
+        queryset = self.get_queryset()
+        with transaction.atomic():
+            if obj.order > int(new_order):
+                queryset.filter(
+                    user=request.user,
+                    order__lt=obj.order,
+                    order__gte=new_order
+                ).exclude(
+                    pk=obj.pk
+                ).update(
+                    order=F('order') + 1
+                )
+            else:
+                queryset.filter(
+                    user=request.user,
+                    order__lte=new_order,
+                    order__gt=obj.order
+                ).exclude(
+                    pk=obj.pk
+                ).update(
+                    order=F('order') - 1
+                )
+            obj.order = new_order
+            obj.save()
 
 
 class Category(models.Model):
