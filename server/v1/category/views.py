01d20daf6c1a027c5ef3871a5a2f9c3e5679c549
chohongjae <bjq913@naver.com>
2020-04-22 01:07:39 +0900

    todo 제거

--- server/v1/category/views.py
+++ server/v1/category/views.py
@@ -46,4 +46,3 @@ class CategoryRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView
         return super().update(request, *args, **kwargs)
 
     # TODO 1) db 실행계획, 인덱스 확인하기
-    # TODO 2) 즐겨찾기를 해제하면서 순서조정을 할 때는 어떻게 하지?
