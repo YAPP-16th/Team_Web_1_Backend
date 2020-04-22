03073420b3a53b1a5f4fd2775ba5a1390696cbb9
DONGJoo Ha <ha3569@gmail.com>
2020-04-23 01:06:31 +0900

    2020-04-23 하동주

--- server/models/url.py
+++ server/models/url.py
@@ -1,5 +1,18 @@
 from django.db import models
-
+from server.models.category import Category
+from server.models.user import User
+from django.conf import settings
+from django.db import models, transaction
+from django.db.models import F
+from rest_framework import serializers
+from server.exceptions import ServerException
 
 class Url(models.Model):
-    pass
+    url = models.CharField(max_length=300)
+    title = models.CharField(max_length=300)
+    description = models.CharField(max_length=350)
+    image = models.CharField(max_length=350)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='url_users', on_delete=models.CASCADE)
+    category = models.ForeignKey(settings.AUTH_CATEGORY_MODEL, related_name='url_categorys', on_delete=models.CASCADE)
