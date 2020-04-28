473a97423717106537cd5d02ef8ea0c0080f6703
DONGJoo Ha <ha3569@gmail.com>
2020-04-28 21:27:35 +0900

    다른 작업을 위해 url 모델 먼저 추가

--- server/models/url.py
+++ server/models/url.py
@@ -1,18 +1,23 @@
-from django.db import models
 from server.models.category import Category
-from server.models.user import User
+
 from django.conf import settings
-from django.db import models, transaction
-from django.db.models import F
+from django.db import models
+
 from rest_framework import serializers
-from server.exceptions import ServerException
+
 
 class Url(models.Model):
     url = models.CharField(max_length=300)
-    title = models.CharField(max_length=300)
-    description = models.CharField(max_length=350)
-    image = models.CharField(max_length=350)
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    image = models.CharField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='url_users', on_delete=models.CASCADE)
-    category = models.ForeignKey(settings.AUTH_CATEGORY_MODEL, related_name='url_categorys', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='urls', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name='urls', on_delete=models.CASCADE)
+
+
+class UrlSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Url
+        fields = '__all__'
