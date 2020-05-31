39739441d763089cb9250eea74fb9edac567f2de
chohongjae <bjq913@naver.com>
2020-06-01 00:56:59 +0900

    어드민페이지에 알람메시지 추가 및 유저 가입타입 구분 추가

--- server/admin.py
+++ server/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 
-from server.models.alarm import Alarm
+from server.models.alarm import Alarm, AlarmMessage
 from server.models.category import Category
 from server.models.url import Url
 
@@ -9,10 +9,11 @@ User = get_user_model()
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'password', 'username', 'is_superuser', 'date_joined')
+    list_display = ('sign_up_type', 'email', 'username', 'is_superuser', 'date_joined')
 
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Category)
 admin.site.register(Url)
 admin.site.register(Alarm)
+admin.site.register(AlarmMessage)
