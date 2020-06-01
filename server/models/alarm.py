df40577f8c2cd320ed773e36239c05894910971d
chohongjae <bjq913@naver.com>
2020-06-01 16:18:54 +0900

    알람메세지 모델 제거하고 알람모델에 보낸메세지인지 boolean필드 추가

--- server/models/alarm.py
+++ server/models/alarm.py
@@ -11,6 +11,7 @@ class Alarm(models.Model):
     category = models.ForeignKey(Category, related_name='alarms', on_delete=models.CASCADE)
     url = models.ForeignKey(Url, related_name='alarms', on_delete=models.CASCADE)
     name = models.CharField(max_length=10)
+    has_been_sent = models.BooleanField(default=False)
     reserved_time = models.DateTimeField(help_text='The time you want the status to change')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -26,17 +27,3 @@ class AlarmSerializer(serializers.ModelSerializer):
     class Meta:
         model = Alarm
         fields = '__all__'
-
-
-class AlarmMessage(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='alarms', on_delete=models.CASCADE)
-    alarm = ''
-    message = ''
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return '{}'.format(self.message)
-
-    class Meta:
-        ordering = ["created_at"]
