5ac2837aa4567dc7197bbc093f2d300bbe75a2eb
chohongjae <bjq913@naver.com>
2020-06-01 16:21:21 +0900

    소켓연결 주소 및 클래스명 변경

--- urlink/routing.py
+++ urlink/routing.py
@@ -2,7 +2,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
 
-from server.v1.alarm.channels.consumer import EchoConsumer
+from server.v1.alarm.channels.consumer import AlarmConsumer
 from .websocket_token_auth import TokenAuthMiddleware
 
 TokenAuthMiddlewareStack = lambda inner: TokenAuthMiddleware(AuthMiddlewareStack(inner))
@@ -11,7 +11,7 @@ application = ProtocolTypeRouter({
     # http->django views is added by default
     'websocket': TokenAuthMiddlewareStack(
         URLRouter([
-            path('api/v1/ws/connection/', EchoConsumer)
+            path('ws/connection/', AlarmConsumer)
         ])
     ),
 })
