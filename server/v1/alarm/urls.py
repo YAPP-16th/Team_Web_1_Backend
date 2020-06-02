02e685f24c4e791c776e3ceb6f562aeaf2e18f56
chohongjae <bjq913@naver.com>
2020-06-02 16:22:24 +0900

    알람 테스트 코드 추가

--- server/v1/alarm/urls.py
+++ server/v1/alarm/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import AlarmListCreateAPIView, AlarmRetrieveUpdateDestroyAPIView
+from .views import AlarmListCreateAPIView, AlarmDestroyAPIView
 
 app_name = 'alarm'
 urlpatterns = [
     path('', AlarmListCreateAPIView.as_view(), name='list_create'),
-    path('<int:pk>/', AlarmRetrieveUpdateDestroyAPIView.as_view(), name='detail'),
+    path('<int:pk>/', AlarmDestroyAPIView.as_view(), name='delete'),
 ]
