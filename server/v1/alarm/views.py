81918b081cda7a420e1ffbb954b163e269e7bbe2
chohongjae <bjq913@naver.com>
2020-06-03 17:51:51 +0900

    알람 뷰 리팩토링

--- server/v1/alarm/views.py
+++ server/v1/alarm/views.py
@@ -1,10 +1,24 @@
+import json
 from datetime import datetime
 
+from django.shortcuts import render
+from django.utils.safestring import mark_safe
 from rest_framework import generics
 from rest_framework import permissions
 
+from server.exceptions import ServerException
 from server.models.alarm import Alarm, AlarmSerializer
 from server.permissions import IsOwner
+
+
+def index(request):
+    return render(request, 'alarm/index.html', {})
+
+
+def room(request, room_name):
+    return render(request, 'alarm/room.html', {
+        'room_name_json': mark_safe(json.dumps(room_name))
+    })
 
 
 class AlarmListCreateAPIView(generics.ListCreateAPIView):
@@ -15,12 +29,13 @@ class AlarmListCreateAPIView(generics.ListCreateAPIView):
         ## Headers
             - Content type : application/json
             - Authorization : JWT <토큰>
+        ## Query Params
+            - category : 카테고리 ID [필수]
+            - url : URL ID [필수]
         ## Body
-            - category : 카테고리 id
-            - url : url id
-            - reserved_time : 예약 시간
+            - reserved_time : {"year": "연도", "month": "월", "day": "일", "hour": "시간(24시간 형식)", "minute": "분"}
+                - 05월 == 5월, 3분 == 03분 - "0" 유무 상관 X
             - name : 알람명
-
     """
     serializer_class = AlarmSerializer
 
@@ -31,18 +46,39 @@ class AlarmListCreateAPIView(generics.ListCreateAPIView):
 
     def post(self, request, *args, **kwargs):
         user = self.request.user.pk
+        category_id = self.request.query_params.get('category')
+        url_id = self.request.query_params.get('url')
         request.data['user'] = user
-        request.data['reserved_time'] = datetime.strptime('{}-{}-{} {}:{}'.format(request.data['reserved_time']['year'],
-                                                                                  request.data['reserved_time']['month'],
-                                                                                  request.data['reserved_time']['day'],
-                                                                                  request.data['reserved_time']['hour'],
-                                                                                  request.data['reserved_time']['minute']),
-                                                          '%Y-%m-%d %H:%M')
+        request.data['category'] = category_id
+        request.data['url'] = url_id
+        request.data['reserved_time'] = self.reformat_string_to_datetime(request.data.get('reserved_time'))
 
         return self.create(request, *args, **kwargs)
 
+    @staticmethod
+    def reformat_string_to_datetime(time):
+        try:
+            _datetime = datetime.strptime(
+                f"{time['year']}-{time['month']}-{time['day']} {time['hour']}:{time['minute']}",
+                '%Y-%m-%d %H:%M')
+        except Exception as e:
+            print(e)
+            raise ServerException('예약시간 형식이 올바르지 않습니다.')
+        else:
+            return _datetime
 
-class AlarmRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+
+class AlarmDestroyAPIView(generics.DestroyAPIView):
+    """
+        Alarm 삭제 API
+
+        ---
+        ## Headers
+            - Content type : application/json
+            - Authorization : JWT <토큰>
+        ## Path Params
+            - id : Alarm ID
+    """
     queryset = Alarm.objects.all()
     serializer_class = AlarmSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwner]
