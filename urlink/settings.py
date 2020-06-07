bf91d79059669fe2e7e2b2440ca3bd81c08313b7
chohongjae <bjq913@naver.com>
2020-06-07 23:46:47 +0900

    static dirs 추가

--- urlink/settings.py
+++ urlink/settings.py
@@ -147,6 +147,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'server/v1/url/static')
+]
 
 SWAGGER_SETTINGS = {
     'SECURITY_DEFINITIONS': {
