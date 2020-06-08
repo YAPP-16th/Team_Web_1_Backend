9bd0a5d3732c290b099524b58864b22c1700a8b5
chohongjae <bjq913@naver.com>
2020-06-08 17:48:15 +0900

    requests timeout 추가 및 get favicon 추가

--- server/v1/url/utils/crawler.py
+++ server/v1/url/utils/crawler.py
@@ -1,11 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
-from server.exceptions import ServerException
-
 
 class Crawler:
-    IMAGE_404 = 'https://www.boostability.com/wp-content/uploads/2012/10/BOOST_BLOG_IMAGE_RB_SET_10_404_PAGE_1200x628px_v1_3.jpg'
+    IMAGE_404 = 'https://urlink.s3.ap-northeast-2.amazonaws.com/static/404-image.png'
+    IMAGE_FAVICON = 'https://urlink.s3.ap-northeast-2.amazonaws.com/static/favicon.png'
 
     def get_html(self, path):
         headers = {
@@ -16,12 +15,10 @@ class Crawler:
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
             'Sec-Fetch-Dest': 'document'
         }
-
-        response = requests.get(path, headers=headers)
-        if not response.ok:
-            raise ServerException(response.reason)
-
-        return response.text
+        try:
+            return requests.get(path, headers=headers, timeout=5).text
+        except Exception as e:
+            return str(e)
 
     def get_meta_data(self, html, keys):
         for key in keys:
@@ -29,23 +26,42 @@ class Crawler:
                 return html.find('meta', key).get('content')
         return '알수없음'
 
+    def get_favicon(self, html, keys):
+        for key in keys:
+            if html.find('link', key) and html.find('link', key).get('href'):
+                return html.find('link', key).get('href')
+        return '알수없음'
+
     def parse_html(self, html):
         soup = BeautifulSoup(html, 'html.parser')
         head = soup.head
 
-        title = head.title.text.strip() if head.title else self.get_meta_data(head,
-                                                                              [{'name': 'title'},
-                                                                               {'property': 'og:title'}])
-        description = self.get_meta_data(head, [{'name': 'description'}, {'property': 'og:description'}]).strip()
+        if not head:
+            return {
+                'title': '알수없음',
+                'description': '알수없음',
+                'image_path': self.IMAGE_404,
+                'favicon_path': self.IMAGE_FAVICON
+            }
+
+        if head.title:
+            title = head.title.text.strip()
+        else:
+            title = self.get_meta_data(head, [{'name': 'title'}, {'property': 'og:title'}])
+        description = self.get_meta_data(head, [{'name': 'description'}, {'property': 'og:description'}])
         image_path = self.get_meta_data(head, [{'property': 'og:image'}, {'property': 'twitter:image'}])
+        favicon_path = self.get_favicon(head, [{'rel': 'shortcut icon'}, {'rel': 'icon'}])
 
         if image_path == '알수없음':
             image_path = self.IMAGE_404
+        if favicon_path == '알수없음':
+            favicon_path = self.IMAGE_FAVICON
 
         return {
             'title': title,
             'description': description,
-            'image_path': image_path
+            'image_path': image_path,
+            'favicon_path': favicon_path
         }
 
 
@@ -53,11 +69,11 @@ if __name__ == "__main__":
     from pprint import pprint
 
     c = Crawler()
-    for path in ['https://programmers.co.kr/learn/challenges?tab=all_challenges', 'https://www.acmicpc.net/',
-                'https://ssungkang.tistory.com/category/%EC%9B%B9%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D/Django',
-                'https://tech.cloud.nongshim.co.kr/techblog/', 'https://mail.google.com/mail/u/0/#inbox',
-                'https://syundev.tistory.com/29?category=868616', 'https://github.com/hotire/turnover-story',
-                'http://www.bloter.net/archives/257437',
-                'https://www.youtube.com/watch?v=r6TFnNQsQLY&feature=youtu.be']:
+    for path in ['abc', 'https://programmers.co.kr/learn/challenges?tab=all_challenges', 'https://www.acmicpc.net/',
+                 'https://ssungkang.tistory.com/category/%EC%9B%B9%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D/Django',
+                 'https://tech.cloud.nongshim.co.kr/techblog/', 'https://mail.google.com/mail/u/0/#inbox',
+                 'https://syundev.tistory.com/29?category=868616', 'https://github.com/hotire/turnover-story',
+                 'http://www.bloter.net/archives/257437',
+                 'https://www.youtube.com/watch?v=r6TFnNQsQLY&feature=youtu.be']:
         html = c.get_html(path)
         pprint(c.parse_html(html))
