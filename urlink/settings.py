fa7e5b1f4346a08f3fd0199d0201c834fd347728
chohongjae <bjq913@naver.com>
2020-06-10 17:15:01 +0900

    server 앱 변경

--- urlink/settings.py
+++ urlink/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'server',
-
+    'server.apps.ServerConfig',
+    # 'server',
     'corsheaders',
 
     'rest_framework',
