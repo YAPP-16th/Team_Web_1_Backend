3aca0fd908096d4be8b88b080f35fc933a58dcea
chohongjae <bjq913@naver.com>
2020-06-24 00:30:38 +0900

    앱실행시에 실행되는 함수 제거

--- server/apps.py
+++ server/apps.py
@@ -1,11 +1,5 @@
 from django.apps import AppConfig
-from django.conf import settings
 
 
 class ServerConfig(AppConfig):
     name = 'server'
-
-    def ready(self):
-        from server.v1.alarm import scheduler
-        if settings.SCHEDULER_AUTOSTART:
-            scheduler.start()
