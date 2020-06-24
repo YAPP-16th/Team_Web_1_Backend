9011c40e9484398364a3d75dcb742c03a5eb4f8e
chohongjae <bjq913@naver.com>
2020-06-24 12:51:13 +0900

    URL Update 추가

--- server/v1/url/urls.py
+++ server/v1/url/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import UrlListCreateAPIView, UrlDestroyAPIView
+from .views import UrlListCreateAPIView, UrlRetrieveUpdateDestroyAPIView
 
 app_name = 'url'
 urlpatterns = [
     path('', UrlListCreateAPIView.as_view(), name='list_create'),
-    path('<int:pk>/', UrlDestroyAPIView.as_view(), name='delete'),
+    path('<int:pk>/', UrlRetrieveUpdateDestroyAPIView.as_view(), name='detail'),
 ]
