8f5e9d54b5906d4dc747e255646487318e8d2a9d
chohongjae <bjq913@naver.com>
2020-10-13 20:29:25 +0900

    snakeCase to camelCase로 변경

--- urlink/settings.py
+++ urlink/settings.py
@@ -59,6 +59,15 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'server.User'
 
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+        'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'djangorestframework_camel_case.parser.CamelCaseFormParser',
+        'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
+        'djangorestframework_camel_case.parser.CamelCaseJSONParser',
+    ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
